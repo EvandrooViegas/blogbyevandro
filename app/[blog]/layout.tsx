d0b829700182ac7a5d24e12237a044a4534ac51e
Evandro Viegas <evandrooviegas@gmail.com>
2024-01-24 23:26:11 +0000

    more changes

--- app/[blog]/layout.tsx
+++ app/[blog]/layout.tsx
@@ -1,4 +1,14 @@
+import { getBlog } from "@/data";
 import React from "react";
+
+export async function generateMetadata({ params }: { params: { blog: string } }) {
+  const b = params.blog
+  const blog = getBlog(b)
+  return {
+    title: blog?.title,
+    description: blog?.body[0].text
+  }
+}
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
